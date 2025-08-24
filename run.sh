a932792e0df8f155939a1ec54562f8fda9157ce6
thedistromaker <pan.savvy.35@gmail.com>
2025-08-24 14:42:56 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cvlc v4l2:///dev/video0:chroma=YUY2:width=640:height=480:fps=30 \
-     --sout '#transcode{vcodec=YUY2,fps=30}:rtp{sdp=rtsp://0.0.0.0:8554/stream}'
+ffmpeg -f v4l2 -input_format mjpeg -framerate 30 -video_size 640x480 -i /dev/video0 \
+       -c:v copy -f rtsp rtsp://0.0.0.0:8554/stream
